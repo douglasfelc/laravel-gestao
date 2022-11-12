b41575acef620c8f28081e24401fa91884c1b955
Douglas Felchilcher <douglasfelc@gmail.com>
2022-11-12 19:06:14 -0300

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'IndexController@index');
+
+Route::get('/about', 'AboutController@about');
+
+Route::get('/contact', 'ContactController@contact');
